cc9b7d4a0c3e3a5a31fb946c84dfc0b3ced61a55
Vilma Hernandez <vilmaglynn@hotmail.com>
2024-01-14 20:04:24 +0000

    changes to hourly forecast to only display 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,116 +1,113 @@
-const key = "a821b4d398ab3031604981a4408fdd33";
-const cityname = "brighton";
-const queryURL1 = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${key}`;
+$(document).ready(function () {
+  $("#search-button").click(function () {
+    event.preventDefault();
+    const cityname = $("#search-input").val();
+    const key = "a821b4d398ab3031604981a4408fdd33";
+    const queryURL1 = `https://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=5&appid=${key}`;
 
-//fetch #1 to get coordinates of lat and lon
-fetch(queryURL1)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    const lat = data[0].lat;
-    const lon = data[0].lon;
-    const country = data[0].country;
-
-    const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
-    //fetch #2 to get the forecast
-    fetch(queryURL2)
+    // Fetch #1 to get coordinates of lat and lon
+    fetch(queryURL1)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        console.log(data.list);
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        const country = data[0].country;
 
-        // Function to organize and display weather forecast by day
-        function displayWeatherData() {
-          const forecastContainer = $("#weatherForecast");
+        const queryURL2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+        // Fetch #2 to get the forecast
+        fetch(queryURL2)
+          .then((response) => response.json())
+          .then((data) => {
+            // Function to organize and display weather forecast by day
+            function displayWeatherData() {
+              const forecastContainer = $("#weatherForecast");
 
-          // Group data by day
-          const groupedByDay = {};
-          data.list.forEach((forecast) => {
-            const day = forecast.dt_txt.split(" ")[0]; // Extracting the date part
-            if (!groupedByDay[day]) {
-              groupedByDay[day] = [];
+              // Group data by day
+              const groupedByDay = {};
+              data.list.forEach((forecast) => {
+                // Extract the date part and format it
+                const dayOfWeek = dayjs(forecast.dt_txt).format("dddd DD");
+
+                // Check if the date is greater than today
+                if (dayjs(forecast.dt_txt).isAfter(dayjs(), "day")) {
+                  if (!groupedByDay[dayOfWeek]) {
+                    groupedByDay[dayOfWeek] = forecast;
+                  }
+                }
+              });
+
+              // Create divs for each day
+              for (const dayOfWeek in groupedByDay) {
+                const dayContainer = $(
+                  `<div class="day-container"><h3>${dayOfWeek}</h3></div>`
+                );
+                const forecast = groupedByDay[dayOfWeek];
+
+                const imageUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
+                const tempKelvin = forecast.main.temp;
+                const tempC = `${Math.round(tempKelvin - 273.15)}°C`;
+
+                // Append image and forecast details to hourlyForecastContainer
+                dayContainer.append(
+                  `<div><img src="${imageUrl}" alt="Weather Icon"><br> ${forecast.weather[0].description} <br>Temp: ${tempC} | Wind: ${forecast.wind.speed} m/ph | Humidity: ${forecast.main.humidity}%</div> <hr>`
+                );
+
+                // Append the day container to the main forecast container
+                forecastContainer.append(dayContainer);
+              }
             }
-            groupedByDay[day].push(forecast);
-          });
-
-          // Create divs for each day
-          for (const day in groupedByDay) {
-            const dayContainer = $(
-              `<div class="day-container"><h3>${day}</h3></div>`
+            // Display TODAY's description on screen
+            const location = `${cityname}, ${country}`;
+            const locationParagraph = $("<h2>").text(location);
+            const description0 = $("<span>").text(
+              data.list[0].weather[0].description
             );
-            const hourlyForecastContainer = $('<div class="hourly-forecast">');
+            $("#today").append(locationParagraph, description0);
 
-            // Add hourly forecast for the day
-            groupedByDay[day].forEach((forecast) => {
-              const imageUrl = `https://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`;
-              const tempKelvin = forecast.main.temp;
-              const tempC = `${Math.round(tempKelvin - 273.15)}°C`;
+            // Function to add an image to the screen
+            const imageUrl = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
+            const image = $("<img>")
+              .attr("src", imageUrl)
+              .attr("alt", "Weather Icon");
+            $("#today").append(image, "<hr>");
 
-              // Append image and forecast details to hourlyForecastContainer
-              hourlyForecastContainer.append(
-                `<div><img src="${imageUrl}" alt="Weather Icon"> ${
-                  forecast.dt_txt.split(" ")[1]
-                } <br>Temp: ${tempC} | Wind: ${
-                  forecast.wind.speed
-                } m/s | Humidity: ${forecast.main.humidity}%</div> <hr>`
-              );
-            });
+            // Display additional weather information (temp, wind, humidity)
+            const tempKelvin = data.list[0].main.temp;
+            const tempC = $("<p>").text(
+              `Temp: ${Math.round(tempKelvin - 273.15)}°C`
+            );
+            const wind = $("<p>").text(`Wind: ${data.list[0].wind.speed} m/ph`);
+            const humidity = $("<p>").text(
+              `Humidity: ${data.list[0].main.humidity}%`
+            );
+            $("#today").append(tempC, wind, humidity);
 
-            // Append the hourly forecast container to the day container
-            dayContainer.append(hourlyForecastContainer);
-
-            // Append the day container to the main forecast container
-            forecastContainer.append(dayContainer);
-          }
-        }
-
-        // display TODAY's description on screen
-        const location = `${cityname}, ${country}`;
-        const locationParagraph = $("<h2>").text(location);
-        const description0 = $("<span>").text(
-          data.list[0].weather[0].description
-        );
-        $("#today").append(locationParagraph, description0);
-
-        // Function to add an image to the screen
-        const imageUrl = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
-        const image = $("<img>")
-          .attr("src", imageUrl)
-          .attr("alt", "Weather Icon");
-        $("#today").append(image, "<hr>");
-
-        // Display additional weather information (temp, wind, humidity)
-        const tempKelvin = data.list[0].main.temp;
-        const tempC = $("<p>").text(
-          `Temp: ${Math.round(tempKelvin - 273.15)}°C`
-        );
-        const wind = $("<p>").text(`Wind: ${data.list[0].wind.speed} m/s`);
-        const humidity = $("<p>").text(
-          `Humidity: ${data.list[0].main.humidity}%`
-        );
-        $("#today").append(tempC, wind, humidity);
-
-        // Call the function to display weather forecast by day
-        displayWeatherData();
+            // Call the function to display weather forecast by day
+            displayWeatherData();
+          });
+      })
+      .catch((error) => {
+        console.log(error);
       });
-  })
-  .catch((error) => {
-    console.log(error);
+
+    // Get current date/time to display on the website main area
+    function displayDateTime() {
+      // Use dayjs to get the current date and time
+      const currentDate = dayjs();
+      // Format the date and time as a string
+      const formattedDateTime = currentDate.format("dddd DD MMM YYYY | HH:mm");
+      // Display the formatted date and time on the screen
+      const txt1 = $("<h2>").text(formattedDateTime);
+      $("#today").append(txt1);
+    }
+
+    // Call the function when the document is ready
+    $(document).ready(function () {
+      displayDateTime();
+    });
+
+    // Clear old data
+    $("#today").empty();
+    $("#weatherForecast").empty();
   });
-
-// Get current date/time to display on the website main area
-function displayDateTime() {
-  // Use dayjs to get the current date and time
-  const currentDate = dayjs();
-  // Format the date and time as a string
-  const formattedDateTime = currentDate.format("ddd DD MMM YYYY | HH:mm");
-  console.log(formattedDateTime);
-  // Display the formatted date and time on the screen
-  const txt1 = $("<h2>").text(formattedDateTime);
-  $("#today").append(txt1);
-}
-
-// Call the function when the document is ready
-$(document).ready(function () {
-  displayDateTime();
 });
